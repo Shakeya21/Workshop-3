db2e61ab137b321349e0c7cc8caff2eff57c3b66
Shakeya21 <Wilczuusiek@gmail.com>
2023-06-02 11:29:54 +0200

    First commit

---
+++ jee-user-crud/src/main/java/pl/coderslab/jeeusercrud/utils/User.java
@@ -0,0 +1,48 @@
+package pl.coderslab.jeeusercrud.utils;
+
+public class User {
+
+    private int id;
+    private String email;
+    private String userName;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String username) {
+        this.userName = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + getId() + " Nazwa Użytkowanika: " + getUserName() +
+                " Email: " + getEmail() + " Hasło: " + getPassword();
+    }
+
+}
